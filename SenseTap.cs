3a0414f2603cd9fcadaa29abce6694edde3b2ae4
z_zabaglione <z.zabaglione@gmail.com>
2014-09-25 21:38:19 +0900

    Oculus Rift DK2のタップを検出するサンプルコード

---
+++ SenseTap.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using OVR;
+
+public class SenseTap : MonoBehaviour {
+	public GameObject box;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Detect a moderate tap on the side of the HMD.
+		Hmd hmd = Hmd.GetHmd();
+		ovrTrackingState ts = hmd.GetTrackingState();
+		if ((ts.StatusFlags & (uint)ovrStatusBits.ovrStatus_OrientationTracked) != 0)
+		{
+			Vector3 v = OVRExtensions.ToVector3(ts.RawSensorData.Accelerometer);
+			// Arbitrary value and representing moderate tap on the side of the DK2 Rift.
+			if (v.sqrMagnitude > 250.0f) {
+				Instantiate(box, box.transform.position, box.transform.rotation);
+			}
+		}
+	}
+}
